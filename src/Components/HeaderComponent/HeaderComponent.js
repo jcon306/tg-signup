4cf6a046aa2075f819790a1c04d0ea879af5eef7
Joe C <consolinojoe@gmail.com>
2023-10-06 15:36:36 -0400

    Minor changes to heading

--- src/Components/HeaderComponent/HeaderComponent.js
+++ src/Components/HeaderComponent/HeaderComponent.js
@@ -15,12 +15,14 @@ const HeaderComponent = () => {
                 <h1>Thanksgiving Giving Sign Up</h1> 
                 <hr /> 
                 <p>
-                    &ensp;Thank you for your willingness to support families in need this Thanksgiving as a partnership with Families for Families.
-                    Your support is critical for feeding the hearts and bellies of many families! Sign up below to sponsor a family for 
-                    Thanksgiving Dinner. Then purchase the ingredients and drop off to 250 Braen Ave in Wyckoff on <strong>November 18th 10-12</strong> 
-                    (detailed instructions will be given after signing up) to be delivered to the families. We are also in need of delivery 
-                    drivers from <strong>1-3</strong> to deliver the meals (<a href="https://give.families4families.com/campaigns/28443-thanksgiving-dinner-drop-off" 
-                    target="_blank" rel="noreferrer" >sign up here</a>). Suggested ingredients include: Boxed Stuffing Mix (like Stovetop), Macaroni & 
+                    Thank you for your willingness to support families in need this Thanksgiving as a partnership with Families for Families.
+                    Your support is critical for feeding the hearts and bellies of many families! <br /> <br />
+                    Sign up below to sponsor a family for Thanksgiving Dinner. <br />
+                    Then purchase the ingredients and drop off to 250 Braen Ave in Wyckoff on <strong>November 18th 10-12 </strong> 
+                    to be delivered to the families (detailed instructions will be given after signing up). <br />
+                    We are also in need of delivery drivers from <strong>1-3</strong> to deliver the meals (<a href="https://give.families4families.com/campaigns/28443-thanksgiving-dinner-drop-off" 
+                    target="_blank" rel="noreferrer" >sign up here</a>). <br /> <br />
+                    Suggested ingredients include: Boxed Stuffing Mix (like Stovetop), Macaroni & 
                     Cheese or Lasagna, Potatoes, Jars of Turkey Gravy or Dried Gravy Mix Packets, Canned Yams, Cranberry Sauce, Canned or fresh Veggies
                     (green beans, corn, peas), Canned or fresh fruit, Cornbread Mix or Biscuit or Roll mix, Frozen Turkey or Turkey Breast 
                     or Frozen Ham, Disposable pan to cook the turkey in, Rice, Red or Black Beans, Canned Pumpkin or Fruit Pie Filling, 
